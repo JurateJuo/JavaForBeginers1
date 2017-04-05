76c52c0576d4d29f309e79f60ccebc0a69e4e9a2
JurateJuo <defliunke@gmail.com>
2017-04-05 19:02:12 +0300

    Initial commit

---
+++ src/FirstExample.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+import java.util.concurrent.SynchronousQueue;
+
+/**
+ * Created by acmenukas on 2017-04-04.
+ */
+public class FirstExample {
+
+    public static void main (String[] arg) {
+        System.out.println("Hi Jurate");
+        FirstExampleSupportClass example = new FirstExampleSupportClass();
+        example.sayHi();
+        int sum = example.sum(10, 20);
+        //System.out.println(example.sum(10, 20));
+        System.out.println(sum);
+
+        Scanner scanner = new Scanner(System.in);
+       /* System.out.println("Please insert a number");
+        int insertedNumb = scanner.nextInt();
+        System.out.print("Inserted number >>" + insertedNumb);*/
+
+      /* example.userName(scanner);*/
+      /* example.userNameLenght(scanner);*/
+        example.userNameVeiksmai(scanner);
+        System.out.println(sum);
+
+
+
+    }
+
+}
+
+class FirstExampleSupportClass{
+
+    public void userNameVeiksmai (Scanner veik){
+        System.out.println("iveskite pirma skaiciu");
+        int first = veik.nextInt();
+        System.out.println("iveskite antra skaiciu");
+        int second = veik.nextInt();
+
+    }
+
+    public void userNameLenght(Scanner jj){
+        System.out.println("Please insert your name");
+        String name = jj.nextLine();
+        System.out.println("Vardo ilgis yra " + name.length() + " raides");
+
+    }
+
+    public void userName(Scanner sc){
+        System.out.println("Please insert your name :)");
+        String name = sc.nextLine();
+        System.out.println("Hi " + name);
+
+    }
+
+    public void sayHi(){
+        System.out.println("Labas");
+    }
+
+    public int sum(int firstNumb, int secondNum){
+        return firstNumb + secondNum;
+    }
+    public int minus(int firstNumb, int secondNum){
+        return firstNumb - secondNum;
+    }
+
+}
+
